ab860bb530984c48f6f2cf2678115387b08bd7b6
ash <asboy2035@icloud.com>
2025-02-25 10:08:46 +0100

    Initial Commit

---
+++ GitFlow/GitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitFlowApp.swift
+//  GitFlow
+//
+//  Created by ash on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitFlowTests/GitFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitFlowTests.swift
+//  GitFlowTests
+//
+//  Created by ash on 2/25/25.
+//
+
+import Testing
+@testable import GitFlow
+
+struct GitFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
